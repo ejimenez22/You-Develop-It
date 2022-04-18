82c04b423d5bd3c9001fdf45d81342ba1093770c
Enrique Jimenez <ejimenez2292@gmail.com>
2022-04-17 20:05:50 -0500

    called back table from DB using db.query

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express()
@@ -6,12 +7,23 @@ const app = express()
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-// routes
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World!'
-    });
-});
+// Connect to Database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySql username
+        user: 'root',
+        // Your MySQL password
+        password: 'RomeoSierraRomeo',
+        database: 'election'
+    },
+
+    console.log('Connected to the election database.')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows)
+})
 
 // Default reponse for any other request (NOT FOUND)
 app.use((req, res) => {
